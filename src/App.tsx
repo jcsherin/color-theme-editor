04439db110a1ad8a6883b8487e4cdaeb87f71e0d
Jacob Sherin <jacob@protoship.io>
2022-03-29 16:23:08 +0530

    List uncategorized colors
    Includes a set of 256 tailwind colors as sample input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { tailwindColors } from "./tailwindAllColors";
+
+type ClassNameProp = { className: string };
+type ColorProps = { color: string } & ClassNameProp;
+
+function ColorSquare({ color, className }: ColorProps) {
+  const style = {
+    backgroundColor: color,
+  };
+  return (
+    <div className={className}>
+      <div className="w-12 h-12 border-2 mx-auto" style={style}></div>
+      <p>{color}</p>
+    </div>
+  );
+}
 
 function App() {
+  const limit = 16;
+  const listItems = tailwindColors
+    .slice(0, limit)
+    .map((color) => (
+      <ColorSquare
+        key={color}
+        color={color}
+        className="flex-column items-center mr-4"
+      />
+    ));
+
   return (
-    <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    <div className="m-4">
+      <p className="mb-2">
+        Showing {listItems.length} of {tailwindColors.length} colors
+        remaining...
+      </p>
+      <div className="flex flex-wrap">{listItems}</div>
+    </div>
   );
 }
 
