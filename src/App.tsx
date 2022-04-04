2b062ec6c78d59d4fe6fa57a4569c6b73697d5fc
Jacob Sherin <jacob@protoship.io>
2022-04-04 17:29:48 +0530

    Add `BatchInput` component for use to input values exported from design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { shuffle } from "./random";
 import { tailwindColors } from "./tailwindAllColors";
 
 type ClassNameProp = { className: string };
@@ -29,6 +31,10 @@ function ColorListItem({ color, handleClick }: ColorProps) {
       </div>
     </div>
   );
+interface BatchInputProps {
+  formData: BatchInputFormat;
+  handleFormUpdate: React.Dispatch<React.SetStateAction<BatchInputFormat>>;
+  handlePopulateExampleData: () => void;
 }
 
 type ColorListProps = {
@@ -51,6 +57,10 @@ function ColorList({ items, handleSelection }: ColorListProps) {
       <div className="flex flex-wrap">{toListItems(items)}</div>
     </div>
   );
+interface TextInputProps {
+  label: string;
+  text: string;
+  handleChange: (event: React.FormEvent<HTMLTextAreaElement>) => void;
 }
 
 type AddColorGroupFormProps = {
@@ -59,6 +69,10 @@ type AddColorGroupFormProps = {
 
 function AddColorGroupForm({
   handleSubmit,
+function TextAreaInput({
+  label,
+  text,
+  handleChange,
   className,
 }: AddColorGroupFormProps & ClassNameProp) {
   const [text, setText] = useState("");
@@ -124,9 +138,18 @@ function SelectableGroup({ groups, handleSelection }: SelectableGroupProps) {
 
 type EmptyStateProps = { message: string };
 function EmptyState({ message }: EmptyStateProps) {
+}: TextInputProps & { className: string }) {
   return (
     <div className="h-16 w-full border-2 border-dotted mb-4 flex items-center justify-center text-red-400 font-semibold">
       <p>{message}</p>
+    <div className={className}>
+      <p className="text-sm font-bold">{label}</p>
+      <textarea
+        className="w-full bg-slate-100 h-60 p-4 border-2 resize-none"
+        placeholder="Enter color group names. One value per line."
+        value={text}
+        onChange={handleChange}
+      ></textarea>
     </div>
   );
 }
@@ -161,6 +184,33 @@ type TailwindConfig = {
     colors: {
       [key: string]: string | TailwindColorMap;
     };
+function BatchInput({
+  formData,
+  handleFormUpdate,
+  handlePopulateExampleData,
+}: BatchInputProps) {
+  const secondaryActionButton =
+    formData.groups.length === 0 && formData.colors.length === 0 ? (
+      <button
+        onClick={handlePopulateExampleData}
+        className="text-blue-600 underline"
+      >
+        Populate Example Data
+      </button>
+    ) : (
+      <button
+        onClick={() => handleFormUpdate({ groups: "", colors: "" })}
+        className="text-blue-600 underline"
+      >
+        Clear Form Data
+      </button>
+    );
+  const handleUpdateGroups = (event: React.FormEvent<HTMLTextAreaElement>) => {
+    const newGroups = event.currentTarget.value;
+    handleFormUpdate((prev) => ({
+      ...prev,
+      groups: newGroups,
+    }));
   };
 };
 
@@ -200,6 +250,12 @@ function ClipboardCopy({ copyText }: ClipboardCopyProps) {
       .catch((err) => {
         console.log(err);
       });
+  const handleUpdateColors = (event: React.FormEvent<HTMLTextAreaElement>) => {
+    const newColors = event.currentTarget.value;
+    handleFormUpdate((prev) => ({
+      ...prev,
+      colors: newColors,
+    }));
   };
 
   const copyAction = isCopied ? (
@@ -217,6 +273,24 @@ function ClipboardCopy({ copyText }: ClipboardCopyProps) {
     <div>
       {copyAction}
       <pre>{copyText}</pre>
+      <div className="block sm:flex">
+        <TextAreaInput
+          className="w-full mr-1 mb-2"
+          label="Color Groups"
+          text={formData.groups}
+          handleChange={handleUpdateGroups}
+        />
+        <TextAreaInput
+          className="w-full mb-2"
+          label="Color values"
+          text={formData.colors}
+          handleChange={handleUpdateColors}
+        />
+      </div>
+      <button className="mr-4 px-12 py-2 font-bold text-red-600 hover:text-red-800 bg-red-200 hover:bg-red-400">
+        Reload Color Values & Groups
+      </button>
+      {secondaryActionButton}
     </div>
   );
 }
@@ -245,6 +319,11 @@ function shuffle(arr: string[]) {
     arr[i] = arr[random];
     arr[random] = tmp;
   }
+// const exampleColorValues = shuffle(tailwindColors).slice(0, 40);
+const exampleColorValues = shuffle(tailwindColors).slice(0, 40);
+interface BatchInputFormat {
+  groups: string;
+  colors: string;
 }
 
 function App() {
@@ -337,6 +416,10 @@ function App() {
     colorGroups,
     colorItems.filter((item) => !item.grouped)
   );
+  const [batch, setBatch] = useState<BatchInputFormat>({
+    groups: "",
+    colors: "",
+  });
 
   return (
     <div className="m-4">
@@ -377,6 +460,15 @@ function App() {
       <ColorList
         items={colorItems.filter((item) => !item.grouped)}
         handleSelection={handleColorItemSelection}
+      <BatchInput
+        formData={batch}
+        handleFormUpdate={setBatch}
+        handlePopulateExampleData={() =>
+          setBatch({
+            groups: exampleColorGroups.join("\n"),
+            colors: exampleColorValues.join("\n"),
+          })
+        }
       />
 
       {groupedColors}
