df6ca6e31d936e44e4a5a8ca991e0675e32a00e5
Jacob Sherin <jacob@protoship.io>
2022-05-10 15:34:21 +0530

    add index for grouping exports

---
+++ src/grouping/index.ts
@@ -0,0 +1,13 @@
+export { ColorSquare } from "./ColorSquare";
+export {
+  allGrouped,
+  ColorListItem,
+  groupSelected,
+  isSelected,
+  makeColorListItem,
+  notGrouped,
+  someSelected,
+  toggleStatus,
+  ungroup,
+} from "./colorListItem";
+export { ColorGroupButton } from "./ColorGroupButton";
