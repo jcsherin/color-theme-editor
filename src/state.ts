c28ae982affc622b8415f6627dc6438600e85f2c
Jacob Sherin <jacob@protoship.io>
2022-05-12 16:15:43 +0530

    extract reducer

--- src/state.ts
+++ src/state.ts
@@ -4,13 +4,20 @@ import {
   notGrouped,
   parseColorGroups,
   makeSelectable,
+  toggleStatus,
+  ungroup,
+  groupSelected,
+  isSelected,
+  GroupDict,
 } from "./grouping";
 import {
+  ColorDict,
   compareColorId,
   getColorName,
   getColorValue,
   HexColor,
   parseColors,
+  updateColorName,
 } from "./color";
 import { UnparsedColorTheme } from "./input";
 
@@ -68,3 +75,150 @@ export function parse(unparsedColorTheme: UnparsedColorTheme): State {
     colorList: colorList,
   };
 }
+
+interface ActionParse {
+  kind: "parse";
+  unparsedColorTheme: UnparsedColorTheme;
+}
+
+interface ActionAddToGroup {
+  kind: "addToGroup";
+  groupName: string;
+}
+
+interface ActionRemoveFromGroup {
+  kind: "removeFromGroup";
+  groupName: string;
+  colorId: string;
+}
+
+interface ActionRenameColor {
+  kind: "renameColor";
+  colorId: string;
+  newName: string;
+}
+
+interface ActionToggleStatus {
+  kind: "toggleStatus";
+  selectableItem: SelectableItem;
+}
+
+interface ActionReset {
+  kind: "reset";
+}
+
+type Action =
+  | ActionParse
+  | ActionAddToGroup
+  | ActionRemoveFromGroup
+  | ActionRenameColor
+  | ActionToggleStatus
+  | ActionReset;
+
+export const getInitialState = (reset: boolean = false) => {
+  const key = "state";
+
+  if (reset) {
+    const emptyJSON = JSON.stringify({
+      colorDict: [],
+      colorGroupDict: [],
+      colorList: [],
+    });
+    localStorage.setItem(key, emptyJSON);
+  }
+
+  const cached = localStorage.getItem(key);
+  if (!cached) {
+    return {
+      colorDict: new Map(),
+      colorGroupDict: new Map(),
+      colorList: [],
+    };
+  }
+
+  const state = JSON.parse(cached);
+  const colorDict: ColorDict = new Map(state.colorDict);
+  const colorGroupDict: GroupDict = new Map(state.colorGroupDict);
+  const colorList: SelectableItem[] = state.colorList;
+  return {
+    colorDict: colorDict,
+    colorGroupDict: colorGroupDict,
+    colorList: colorList,
+  };
+};
+
+export const reducer = (state: State, action: Action): State => {
+  switch (action.kind) {
+    case "parse": {
+      // Don't reparse user input!
+      if (state.colorDict.size > 0 && state.colorGroupDict.size > 0)
+        return state;
+
+      return parse(action.unparsedColorTheme);
+    }
+
+    case "addToGroup": {
+      const group = state.colorGroupDict.get(action.groupName);
+      if (!group) return state;
+
+      const selected = state.colorList
+        .filter(isSelected)
+        .map((item) => item.colorId);
+
+      const deduped = new Set([...group.colorIds, ...selected]);
+      const newGroup = { ...group, colorIds: Array.from(deduped) };
+      state.colorGroupDict.set(group.name, newGroup);
+
+      return {
+        ...state,
+        colorGroupDict: new Map(Array.from(state.colorGroupDict)),
+        colorList: groupSelected(state.colorList),
+      };
+    }
+
+    case "removeFromGroup": {
+      const group = state.colorGroupDict.get(action.groupName);
+      if (!group) return state;
+
+      const colorIds = group.colorIds.filter(
+        (colorId) => colorId !== action.colorId
+      );
+      const newGroup = { ...group, colorIds: colorIds };
+      state.colorGroupDict.set(group.name, newGroup);
+
+      return {
+        ...state,
+        colorGroupDict: new Map(Array.from(state.colorGroupDict)),
+        colorList: state.colorList.map(ungroup(action.colorId)),
+      };
+    }
+
+    case "renameColor": {
+      const color = state.colorDict.get(action.colorId);
+      if (!color) return state;
+
+      state.colorDict.set(
+        action.colorId,
+        updateColorName(color, action.newName)
+      );
+
+      return {
+        ...state,
+        colorDict: new Map(Array.from(state.colorDict)),
+      };
+    }
+
+    case "toggleStatus": {
+      return {
+        ...state,
+        colorList: state.colorList.map(
+          toggleStatus(action.selectableItem.colorId)
+        ),
+      };
+    }
+
+    case "reset": {
+      return getInitialState(true);
+    }
+  }
+};
