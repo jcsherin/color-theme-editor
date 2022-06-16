18192cd086a3292d98e02a6484ab3ccf93395765
Jacob Sherin <jacob@protoship.io>
2022-06-16 13:01:44 +0530

    2-column layout

--- src/theme-editor/ThemeEditor.tsx
+++ src/theme-editor/ThemeEditor.tsx
@@ -66,72 +66,68 @@ export function ThemeEditor({
 }) {
   const [groupingMode, setGroupingMode] = useState<GroupingMode>("submitForm");
 
-  const groupingAction = (groupingMode: GroupingMode) => {
-    switch (groupingMode) {
-      case "submitForm": {
-        return (
-          <button
-            onClick={(_event) => setGroupingMode("updateForm")}
-            className="justify-self-end ml-auto py-1 px-4 rounded-sm bg-sky-900 hover:bg-sky-700 text-sky-50"
-          >
-            Update
-          </button>
-        );
-      }
-      case "updateForm": {
-        return (
-          <button
-            onClick={(_event) => setGroupingMode("submitForm")}
-            className="justify-self-end ml-auto py-1 px-4 rounded-sm bg-sky-900 hover:bg-sky-700 text-sky-50"
-          >
-            Submit
-          </button>
-        );
-      }
-    }
-  };
-
   const groupingView = (groupingMode: GroupingMode) => {
     switch (groupingMode) {
       case "submitForm":
         return (
-          <GroupColors
-            state={state}
-            handleSelection={handleToggleStatus}
-            handleAddToGroup={handleAddToGroup}
-          />
+          <div>
+            <div className="h-10 mb-4 flex items-center">
+              <button
+                onClick={(_event) => setGroupingMode("updateForm")}
+                className="justify-self-end ml-auto py-1 px-4 rounded-sm bg-sky-900 hover:bg-sky-700 text-sky-50"
+              >
+                Update
+              </button>
+            </div>
+            <GroupColors
+              state={state}
+              handleSelection={handleToggleStatus}
+              handleAddToGroup={handleAddToGroup}
+            />
+          </div>
         );
       case "updateForm":
         return (
-          <Form form={state.formData} handleUpdateForm={handleMergeState} />
+          <div>
+            <div className="h-10 mb-4 flex items-center">
+              <button
+                onClick={(_event) => setGroupingMode("submitForm")}
+                className="justify-self-end ml-auto py-1 px-4 rounded-sm bg-sky-900 hover:bg-sky-700 text-sky-50"
+              >
+                Submit
+              </button>
+            </div>
+            <Form form={state.formData} handleUpdateForm={handleMergeState} />
+          </div>
         );
     }
   };
 
   return (
     <>
-      <div className="h-10 mb-4 flex items-center">
-        <button
-          onClick={(_event) => handlePrevUI()}
-          className="mr-4 py-1 px-4 rounded-sm bg-green-100 hover:bg-green-300 text-green-500 hover:text-green-700"
-        >
-          Create New
-        </button>
-        <CopyButton
-          label="Copy theme to clipboard"
-          content={serializeForTailwind(state)}
-          expiryInMs={2000}
-          className="mr-4 text-blue-500 hover:text-blue-800"
-          flashClassName="text-green-800"
-        />
-        {groupingAction(groupingMode)}
-      </div>
-      <div className="grid grid-cols-2" style={{ height: `calc(100% - 56px)` }}>
-        <TreeEditor
-          state={state}
-          handleRenameColor={handleRenameColor}
-          handleRemoveFromGroup={handleRemoveFromGroup}
-        />
+      <div className="grid grid-cols-2">
+        <div>
+          <div className="h-10 mb-4 flex items-center">
+            <button
+              onClick={(_event) => handlePrevUI()}
+              className="mr-4 py-1 px-4 rounded-sm bg-green-100 hover:bg-green-300 text-green-500 hover:text-green-700"
+            >
+              Create New
+            </button>
+            <CopyButton
+              label="Copy theme to clipboard"
+              content={serializeForTailwind(state)}
+              expiryInMs={2000}
+              className="mr-4 text-blue-500 hover:text-blue-800"
+              flashClassName="text-green-800"
+            />
+          </div>
+          <TreeEditor
+            state={state}
+            handleRenameColor={handleRenameColor}
+            handleRemoveFromGroup={handleRemoveFromGroup}
+          />
+        </div>
         {groupingView(groupingMode)}
       </div>
     </>
