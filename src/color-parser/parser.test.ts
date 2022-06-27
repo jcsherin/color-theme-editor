5f2ef5732c2a7a0fa26c05fa442af479ad7e8c56
Jacob Sherin <jacob@protoship.io>
2022-06-27 19:23:24 +0530

    move snapshots to external file

--- src/color-parser/parser.test.ts
+++ src/color-parser/parser.test.ts
@@ -24,63 +24,19 @@ describe("Color Parser", () => {
   it("returns a parser error for non-existent color keywords", () => {
     const expected = parse("notarealcolorkeyword");
 
-    expect(expected).toMatchInlineSnapshot(`
-Object {
-  "message": "notarealcolorkeyword:  is not a supported color format.
-The supported formats are: hex, named  color keywords, rgb(), rgba(), hsl() and hsla().",
-  "value": "notarealcolorkeyword",
-}
-`);
+    expect(expected).toMatchSnapshot();
   });
 
   it("parses hex values of size 8, 6, 4 & 3", () => {
-    expect(parse("#4682B4FF")).toMatchInlineSnapshot(`
-Object {
-  "tag": "hex-color",
-  "value": "#4682B4FF",
-}
-`);
-    expect(parse("#4682B4")).toMatchInlineSnapshot(`
-Object {
-  "tag": "hex-color",
-  "value": "#4682B4",
-}
-`);
-    expect(parse("#4682")).toMatchInlineSnapshot(`
-Object {
-  "tag": "hex-color",
-  "value": "#4682",
-}
-`);
-    expect(parse("#468")).toMatchInlineSnapshot(`
-Object {
-  "tag": "hex-color",
-  "value": "#468",
-}
-`);
+    expect(parse("#4682B4FF")).toMatchSnapshot();
+    expect(parse("#4682B4")).toMatchSnapshot();
+    expect(parse("#4682")).toMatchSnapshot();
+    expect(parse("#468")).toMatchSnapshot();
   });
 
   it("returns a parse error for invalid hex values", () => {
-    expect(parse("#46")).toMatchInlineSnapshot(`
-Object {
-  "message": "#46:  is not a supported color format.
-The supported formats are: hex, named  color keywords, rgb(), rgba(), hsl() and hsla().",
-  "value": "#46",
-}
-`);
-    expect(parse("#4682B4FF00")).toMatchInlineSnapshot(`
-Object {
-  "message": "#4682B4FF00:  is not a supported color format.
-The supported formats are: hex, named  color keywords, rgb(), rgba(), hsl() and hsla().",
-  "value": "#4682B4FF00",
-}
-`);
-    expect(parse("#xyz")).toMatchInlineSnapshot(`
-Object {
-  "message": "#xyz:  is not a supported color format.
-The supported formats are: hex, named  color keywords, rgb(), rgba(), hsl() and hsla().",
-  "value": "#xyz",
-}
-`);
+    expect(parse("#46")).toMatchSnapshot();
+    expect(parse("#4682B4FF00")).toMatchSnapshot();
+    expect(parse("#xyz")).toMatchSnapshot();
   });
 });
